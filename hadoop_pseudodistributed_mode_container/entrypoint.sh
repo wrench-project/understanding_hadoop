4f749c8969bdf4e9df48c5dd1efaddd5361f7589
ryan <ryanyt@hawaii.edu>
2019-01-13 20:32:34 -1000

    cleaned up 'number of map tasks' test and added a few more test cases

--- hadoop_pseudodistributed_mode_container/entrypoint.sh
+++ hadoop_pseudodistributed_mode_container/entrypoint.sh
@@ -3,16 +3,5 @@
 # start sshd
 /etc/init.d/ssh restart
 
-# setup environment
-su hadoop -c "$HADOOP_HOME/bin/hdfs namenode -format \
-              && $HADOOP_HOME/sbin/start-dfs.sh \
-              && $HADOOP_HOME/sbin/start-yarn.sh \
-              && $HADOOP_HOME/bin/hdfs dfs -mkdir /user \
-              && $HADOOP_HOME/bin/hdfs dfs -mkdir /user/hadoop"
-
-echo "Current Running Java Processes"
-echo "------------------------------"
-jps
-
-# this is so that CMD can run after ENTRYPOINT 
+# this is so that CMD can run after ENTRYPOINT
 exec "$@"
