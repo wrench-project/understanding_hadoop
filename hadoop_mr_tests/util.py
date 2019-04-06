a188e96d4d9128e542b30eb0ab3db2c00c9764af
ryan <ryanyt@hawaii.edu>
2019-04-05 16:00:07 -1000

    more hadoop utilities

--- hadoop_mr_tests/util.py
+++ hadoop_mr_tests/util.py
@@ -5,19 +5,27 @@ import subprocess
 import shutil
 import re
 
-def print_purple(a): print("\033[95m{}\033[00m".format(a))
-def print_red(a): print("\033[91m{}\033[00m".format(a))
-def print_blue(a): print("\033[94m{}\033[00m".format(a))
+def print_purple(a, **kwargs): print("\033[95m{}\033[00m".format(a), **kwargs)
+def print_red(a, **kwargs): print("\033[91m{}\033[00m".format(a), **kwargs)
+def print_blue(a, **kwargs): print("\033[94m{}\033[00m".format(a), **kwargs)
 
 def MiB_to_bytes(MiB): return MiB * 1024 * 1024
 def bytes_to_MiB(bytes): return bytes / (1024 * 1024)
 
-def generate_character_file(num_characters):
+# the directory to write any input files to before they are added to HDFS
+INPUT_DIRECTORY = "/home/hadoop/input"
+
+# paths to hadoop java apps
+HADOOP_HOME = "/usr/local/hadoop"
+HADOOP = HADOOP_HOME + "/bin/hadoop"
+HDFS = HADOOP_HOME + "/bin/hdfs"
+YARN = HADOOP_HOME + "/bin/yarn"
+
+def make_input_directory():
     """
-    Generates an input directory with a single file of num_char characters. (not including \n)
+    Creates the directory INPUT_DIRECTORY, which is used to hold any
+    input files.
     """
-    # generate input files
-    INPUT_DIRECTORY = "/home/hadoop/input"
     try:
         # if the directory is already there, delete it and its contents
         if os.path.exists(INPUT_DIRECTORY):
@@ -29,36 +37,45 @@ def generate_character_file(num_characters):
         print("Script failed..")
         sys.exit()
 
+
+def hdfs_generate_character_file(num_characters):
+    """
+    Generates a single file named "input_file" of num_char characters (not including \n) in
+    INPUT_DIRECTORY, then adds this file to HDFS.
+    For example, calling "path = generate_character_file(2)" will generate a file
+    INPUT_DIRECTORY/input_file with the contents "r\\nr\\n", then place it in HDFS.
+    """
+    make_input_directory()
+
+    # create the file
+    print_red("generating character file")
+
     os.chdir(INPUT_DIRECTORY)
-    with open("input_file", "w") as f:
+    FILE = "input_file"
+    with open(FILE, "w") as f:
         words = ["r\n" for w in range(num_characters)]
         for word in words:
             f.write(word)
 
     print("File Size: {} MiB, Number of Words: {}".format(bytes_to_MiB(num_characters * 2), num_characters))
-    print("Path to inputs: {}".format(INPUT_DIRECTORY))
 
-    return INPUT_DIRECTORY
+    # add the file to HDFS
+    generate_input = execute_command(HDFS + " dfs -put " + INPUT_DIRECTORY, stderr=subprocess.DEVNULL)
+    print(generate_input.decode())
 
 
-def generate_inputs(num_files, file_size_in_MiB):
+def hdfs_generate_word_files(num_files, file_size_in_MiB):
     """
-    Generates an input directory with a specified number of same sized files
-    and returns the path to that directory.
+    Generates num_files file(s) each of size file_size_in_MiB in INPUT_DIRECTORY,
+    then adds the files to HDFS. Each file contains the same word. For example
+    "path = generate_word_files(2, 2)" will generate two files, each being 2 MiB
+    and containing all the same word (each word is 'w' repeated some number of
+    times such that we get the desired file size).
     """
+    make_input_directory()
 
-    # generate input files
-    INPUT_DIRECTORY = "/home/hadoop/input"
-    try:
-        # if the directory is already there, delete it and its contents
-        if os.path.exists(INPUT_DIRECTORY):
-            shutil.rmtree(INPUT_DIRECTORY)
-
-        os.mkdir(INPUT_DIRECTORY)
-    except OSError:
-        print("Unable to create directory at {}".format(INPUT_DIRECTORY))
-        print("Script failed..")
-        sys.exit()
+    # create the files
+    print_red("generating word files")
 
     os.chdir(INPUT_DIRECTORY)
     WORD_SIZE = 1000 # characters
@@ -69,9 +86,22 @@ def generate_inputs(num_files, file_size_in_MiB):
                 f.write(word)
 
     print("Input files generated: {}, File Size: {} MiB".format(num_files, file_size_in_MiB))
-    print("Path to inputs: {}".format(INPUT_DIRECTORY))
 
-    return INPUT_DIRECTORY
+    # add the files to HDFS
+    generate_inputs = execute_command(HDFS + " dfs -put " + INPUT_DIRECTORY, stderr=subprocess.DEVNULL)
+    print(generate_inputs.decode())
+
+
+def execute_command(command, **kwargs):
+    """
+    Wrapper around subprocess.check_output().
+    Executes command by spawning a process to be run as the "hadoop" user
+    and waiting for it to return its output.
+    """
+    print_purple("executing: ", end='')
+    print(command)
+    return subprocess.check_output(["su", "hadoop", "-c", command], **kwargs)
+
 
 def hadoop_start_up():
     """
@@ -81,43 +111,33 @@ def hadoop_start_up():
     3. starts YARN
     4. sets up a directory for user 'hadoop' in HDFS DFS
     """
-
-    # format the namenode
+    # format the namenode, the "master" in HDFS, which manages the filesystem namespace,
+    # and knows what blocks reside at each datanode
     print_red("formatting the namenode")
-    format_namenode = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/bin/hdfs namenode -format -y"],
-                                                stderr=subprocess.DEVNULL)
+    format_namenode = execute_command(HDFS + " namenode -format -y", stderr=subprocess.DEVNULL)
     print(format_namenode.decode())
 
     # start up hdfs
     print_red("starting hdfs")
-    start_hdfs = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/sbin/start-dfs.sh"],
-                                            stderr=subprocess.DEVNULL)
+    start_hdfs = execute_command(HADOOP_HOME + "/sbin/start-dfs.sh", stderr=subprocess.DEVNULL)
     print(start_hdfs.decode())
-
-    # show hdfs block size
-    print_red("hdfs block size")
-    show_block_size = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/bin/hdfs getconf -confKey dfs.blocksize"],
-                                                stderr=subprocess.DEVNULL)
-    print("{} MiB \n".format(int(show_block_size.decode()) / (1024 * 1024)))
 
     # start yarn
     print_red("starting yarn")
-    start_yarn = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/sbin/start-yarn.sh"],
-                                        stderr=subprocess.DEVNULL)
+    start_yarn = execute_command(HADOOP_HOME + "/sbin/start-yarn.sh", stderr=subprocess.DEVNULL)
     print(start_yarn.decode())
 
     # show currently running jvms
-    # if we are correctly running in pseudodistributed mode, we should see the following: Jps, ResourceManager, SecondaryNameNode, NodeManager, DataNode, NameNode
+    # if we are correctly running in pseudodistributed mode, we should see the following:
+    # Jps, ResourceManager, SecondaryNameNode, NodeManager, DataNode, NameNode
     print_red("currently running jvms")
-    jps = subprocess.check_output(["jps"])
+    jps = execute_command("/usr/local/java/bin/jps", stderr=subprocess.DEVNULL)
     print(jps.decode())
 
-    # create directory for hadoop user in hdfs
+    # create directory for hadoop user in hdfs at /user/hadoop
     print_red("creating hdfs folder for the hadoop user")
-    dfs_mkdir_user = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/bin/hdfs dfs -mkdir /user"],
-                                            stderr=subprocess.DEVNULL)
-    dfs_mkdir_user_hadoop = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/bin/hdfs dfs -mkdir /user/hadoop"],
-                                                    stderr=subprocess.DEVNULL)
+    execute_command(HDFS + " dfs -mkdir /user", stderr=subprocess.DEVNULL)
+    execute_command(HDFS + " dfs -mkdir /user/hadoop", stderr=subprocess.DEVNULL)
     print("/user/hadoop created in hdfs \n")
 
 
@@ -130,44 +150,48 @@ def hadoop_tear_down():
     """
     # stop hdfs
     print_red("stopping hdfs")
-    stop_hdfs = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/sbin/stop-dfs.sh"],
-                                        stderr=subprocess.DEVNULL)
+    stop_hdfs = execute_command(HADOOP_HOME + "/sbin/stop-dfs.sh", stderr=subprocess.DEVNULL)
     print(stop_hdfs.decode())
 
     # stop yarn
     print_red("stopping yarn")
-    stop_yarn = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/sbin/stop-yarn.sh"],
-                                        stderr=subprocess.DEVNULL)
+    stop_yarn = execute_command(HADOOP_HOME + "/sbin/stop-yarn.sh", stderr=subprocess.DEVNULL)
     print(stop_yarn.decode())
 
     # show currently running jvms
     # if we are correctly running in pseudodistributed mode, we should see the following: Jps, ResourceManager, SecondaryNameNode, NodeManager, DataNode, NameNode
     print_red("check that no jvms running hadoop daemons are present")
-    jps = subprocess.check_output(["jps"])
+    jps = execute_command("/usr/local/java/bin/jps", stderr=subprocess.DEVNULL)
     print(jps.decode())
 
     # cleanup
     print_red("cleaning up /tmp/hadoop-*")
     rm_tmp = subprocess.check_output(["rm", "-rf", "/tmp/hadoop-hadoop", "/tmp/hadoop-yarn-hadoop"])
 
-def hadoop_print_configuration_property_value(property):
+
+def hadoop_print_configuration_property_values(*properties):
     """
-    Prints the configuration value for a provided property.
+    Prints the configuration values for the given properties.
     """
+    print_red("configuration property values")
+
     # get the property value and print it to stdout
-    property_value = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/bin/hadoop org.apache.hadoop.hdfs.tools.GetConf -confKey {}".format(property)])
-    print("property: {}, value: {}".format(property, property_value.decode()))
+    for property in properties:
+        property_value = execute_command(HADOOP + " org.apache.hadoop.hdfs.tools.GetConf -confKey " + property, stderr=subprocess.DEVNULL)
+        print("property: {}, value: {}".format(property, property_value.decode()))
+
 
 def yarn_get_application_id():
     """
     Returns the applicationId with state FINISHED.
     """
-    # get the id of the application that was just run from yarn
-    yarn_app_list = subprocess.check_output(["su", "hadoop", "-c", "/usr/local/hadoop/bin/yarn application -list -appStates FINISHED"],
-                                            stderr=subprocess.DEVNULL)
+    # get the list of applications with state FINISHED from YARN
+    yarn_app_list = execute_command(YARN + " application -list -appStates FINISHED", stderr=subprocess.DEVNULL)
 
+    # extract the only application_id that should be there
     application_id = re.search(r'application_[0-9]+_[0-9]+', yarn_app_list.decode())
     return application_id.group()
+
 
 def yarn_write_logs_to_file(application_id):
     """
