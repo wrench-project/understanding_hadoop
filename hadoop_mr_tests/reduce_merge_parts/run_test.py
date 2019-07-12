b55cb66f508777e589068d47f38bd4db46e1bbb0
ryan <ryanyt@hawaii.edu>
2019-07-11 16:06:51 -1000

    #2 documentation

--- hadoop_mr_tests/reduce_merge_parts/run_test.py
+++ hadoop_mr_tests/reduce_merge_parts/run_test.py
@@ -5,6 +5,14 @@ import time
 # this module will be placed in the same directory as this file by Dockerfile 'COPY'
 import util
 
+"""
+Test to determine how reduce tasks merge map output files.
+This test is arranged such that there will be n mappers each
+generating single output files of size k where n and k are
+values that we choose. Additionally, only a single reducer
+is run.
+"""
+
 if __name__=="__main__":
     '''
     REDUCE SIDE PROPERTIES
@@ -13,8 +21,8 @@ if __name__=="__main__":
         The number of threads (Fetcher.java or LocalFetcher.java in the source) used to copy
         map outputs to the reducer. Fetcher theads ask a scheduler (ShuffleSchedulerImpl.java)
         for a host to copy map outputs from by calling getHost() (this is synchronized). Therefore
-        it will be useless (and wasteful) to set this value to be more than the number of hosts 
-        available to run map tasks. 
+        it will be useless (and wasteful) to set this value to be more than the number of hosts
+        available to run map tasks.
 
     mapreduce.reduce.shuffle.maxfetchfailures
         The number of times a reducer tries to fetch a map output before reporting the
@@ -42,7 +50,7 @@ if __name__=="__main__":
         copies a map output of about 5MB or less, the map output will be copied to memory. If
         the map output doesn't fit into memory it's currently too full, its contents will be spilled
         and the shuffle should proceed after. If the map output is greater than about 5MB, then
-        the output will be copied to disk. 
+        the output will be copied to disk.
 
     mapreduce.reduce.shuffle.merge.percent
         The threshold usage proportion for the map outputs buffer for starting the process of
@@ -51,25 +59,25 @@ if __name__=="__main__":
     mapreduce.reduce.merge.inmem.threshold
         The threshold number of map outputs for starting the process of merging the outputs and
         spilling to disk. A value of 0 or less means there is no threshold and the spill behavior
-        is goverened by the merge.percent property above. 
+        is goverened by the merge.percent property above.
 
     mapreduce.reduce.input.buffer.percent
-        The proportion of the total heap size to be used for retaining map outputs in memory 
-        during the reduce. For the reduce phase to begin, the size of map outputs in memory must 
-        be no more than this size. By default, all map outputs are merged to disk before the reduce 
-        begins, to give the reducers as much memory as possible. However, if your reducers require 
+        The proportion of the total heap size to be used for retaining map outputs in memory
+        during the reduce. For the reduce phase to begin, the size of map outputs in memory must
+        be no more than this size. By default, all map outputs are merged to disk before the reduce
+        begins, to give the reducers as much memory as possible. However, if your reducers require
         less memory, this value may be increased to minimize the number of trips to disk.
     '''
 
     MAPREDUCE_PROPERTIES = {
         "mapreduce.reduce.memory.mb": 1024, # memory allotted for reduce task container
-        "mapreduce.reduce.java.opts": "-Xmx100m", # reduce jvm process heap size 
-        "mapreduce.reduce.shuffle.parallelcopies": 5, 
-        "mapreduce.task.io.sort.factor": 3, 
-        "mapreduce.reduce.shuffle.input.buffer.percent": 0.1, 
+        "mapreduce.reduce.java.opts": "-Xmx100m", # reduce jvm process heap size
+        "mapreduce.reduce.shuffle.parallelcopies": 5,
+        "mapreduce.task.io.sort.factor": 3,
+        "mapreduce.reduce.shuffle.input.buffer.percent": 0.1,
         "mapreduce.reduce.shuffle.memory.limit.percent": 0.2,
-        "mapreduce.reduce.shuffle.merge.percent" : 0.5, 
-        "mapreduce.reduce.merge.inmem.threshold": 1000, 
+        "mapreduce.reduce.shuffle.merge.percent" : 0.5,
+        "mapreduce.reduce.merge.inmem.threshold": 1000,
         "mapreduce.reduce.input.buffer.percent": 0.5
     }
 
@@ -92,16 +100,16 @@ if __name__=="__main__":
     # Map Output File Sizes < Max Single Shuffle Limit #
     ####################################################
     '''
-    Test: 2 mappers each output 1 MB files. 
-    Result: 
+    Test: 2 mappers each output 1 MB files.
+    Result:
         - Both files are read into memory.
         - 2 MB of input fits completely in memory and does not exceed the merge threshold, so only a single merge should be
-          done from RAM straight into the reduce as 2 MB also fits in the reduce function input buffer size 
+          done from RAM straight into the reduce as 2 MB also fits in the reduce function input buffer size
     '''
     T1 = [["r" for j in range(ONE_MB // TOTAL_BYTES_PER_KV_PAIR)] for i in range(2)]
 
     '''
-    Test: 3 mappers each output 1 MB files. 
+    Test: 3 mappers each output 1 MB files.
     Result:
         - All files are read into memory.
         - The total size of all files will be < the merge threshold so InMemoryMerger won't merge the files.
@@ -113,9 +121,9 @@ if __name__=="__main__":
     '''
     Test: 5 mappers each output 1 MB files.
     Result:
-        - All files are read into memory. 
+        - All files are read into memory.
         - Merge threshold is met as 5 MB > 4.6 MB and InMemoryMerger merges the 5 files into one and spills it onto disk.
-        - The single spill file is fed from disk to the reducer. 
+        - The single spill file is fed from disk to the reducer.
     '''
     T3 = [["r" for j in range(ONE_MB // TOTAL_BYTES_PER_KV_PAIR)] for i in range(5)]
 
@@ -125,7 +133,7 @@ if __name__=="__main__":
         - 5 files are read into memory.
         - Merge threshold is met, then InMemoryMerger merges the 5 files into one and spills it onto disk.
         - Repeat the above 2 steps.
-        - 2 spill files now reside on disk and are fed to the reducer. 
+        - 2 spill files now reside on disk and are fed to the reducer.
     '''
     T4 = [["r" for j in range(ONE_MB // TOTAL_BYTES_PER_KV_PAIR)] for i in range(10)]
 
@@ -138,7 +146,7 @@ if __name__=="__main__":
         - On the fifth time the merge threshold is met, when the InMemoryMergeMerger calls closeOnDiskFile(compressAwarePath),
             the OnDiskMerger is called to merge the on disk files because onDiskMapOutputs.sizez() >= (2 * ioSortFactor - 1).
         - The OnDiskMerger merges 3 files, leaving two so that we end up with 3 total files in the end (ioSortFactor = 3).
-        - The 3 files are fed to the reducer from disk. 
+        - The 3 files are fed to the reducer from disk.
     '''
     T5 = [["r" for j in range(ONE_MB // TOTAL_BYTES_PER_KV_PAIR)] for i in range(25)]
 
@@ -146,12 +154,12 @@ if __name__=="__main__":
     Test: 10 mappers each output 512 KB files.
     Result:
         - 9 files are read into memory.
-        - Merge threshold is met, then InMemoryMerger merges the 9 files into one and spills it onto disk. 
+        - Merge threshold is met, then InMemoryMerger merges the 9 files into one and spills it onto disk.
             Merge factor is not considered for in memory segments, and so in one pass, the 9 files are merged
-            into one. 
-        - 1 file is read into memory. 
-        - There is 1 file in memory that is 512 KB, and 1 file on disk that is about (512 KB * 9) KB. 
-            These files are fed to the reducer as is. 
+            into one.
+        - 1 file is read into memory.
+        - There is 1 file in memory that is 512 KB, and 1 file on disk that is about (512 KB * 9) KB.
+            These files are fed to the reducer as is.
     '''
     T6 = [["r" for j in range((ONE_MB // 2) // TOTAL_BYTES_PER_KV_PAIR)] for i in range(10)]
 
@@ -163,7 +171,7 @@ if __name__=="__main__":
     Result:
         - The file is copied from the mapper, to the disk of the reducer.
         - Even though 3 MB < input buffer size and 3 MB < reduce function input buffer, since 3 MB > max shuffle limit,
-          the file is written to disk. 
+          the file is written to disk.
     '''
     T7 = [["r" for j in range((3 * ONE_MB) // TOTAL_BYTES_PER_KV_PAIR)] for i in range(1)]
 
@@ -181,24 +189,24 @@ if __name__=="__main__":
         - OnDiskMerger thread will start merges when onDiskMapOutputs.size() >= (2 * ioSortFactor - 1),
             so in this case when onDiskMapOutputs.size() >= 5
         - Initially 5 files are copied to the reducer.
-        - OnDiskMerger starts, and 3 files are merged into 1 (2 ignored), therefore we end up with 
+        - OnDiskMerger starts, and 3 files are merged into 1 (2 ignored), therefore we end up with
             3 (ioSortFactor) number of files.
         - 2 more files are copied, and now there are 5 total files on disk. OnDiskMerger starts
-            and 3 files are merged into 1 while 2 are ignored. Now, there are 3 on disk files. 
-            So far, 7 out of the 10 map output files have been copied. 
+            and 3 files are merged into 1 while 2 are ignored. Now, there are 3 on disk files.
+            So far, 7 out of the 10 map output files have been copied.
         - 2 more files are copied, and again there are a total of 5 files on disk. OnDiskMerger
             starts and 3 files are merged into 1 while 2 are ignored. Now there are 3 on disk files.
-            So far, 9 out of the 10 map output files have been copied. 
+            So far, 9 out of the 10 map output files have been copied.
         - The final file is copied leaving a total of 4 on disk files. ioSortFactor is 3, and so the
             final merge (initiated by the MergeManagerImpl) merges the file into 1 of the 3 files
-            from the previous step thus leaving us with 3 on disk files for the reduce. 
+            from the previous step thus leaving us with 3 on disk files for the reduce.
     '''
     T9 = [["r" for j in range((3 * ONE_MB) // TOTAL_BYTES_PER_KV_PAIR)] for i in range(10)]
 
     # set the test 
-    test = T1
+    test = T9
 
-    # ----------------------------------------------------------------------------------- 
+    # -----------------------------------------------------------------------------------
     util.hadoop_start_up()
 
     util.hdfs_generate_custom_word_files(test)
@@ -206,7 +214,7 @@ if __name__=="__main__":
     '''
     util.execute_command(("/usr/local/hadoop/bin/hadoop jar "
                             "/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.0-SNAPSHOT.jar "
-                            "wordcount {} input output").format(" ".join(["-D {}={}".format(property, value) for property, value in MAPREDUCE_PROPERTIES.items()])), 
+                            "wordcount {} input output").format(" ".join(["-D {}={}".format(property, value) for property, value in MAPREDUCE_PROPERTIES.items()])),
                                 stderr=subprocess.STDOUT)
     '''
     subprocess.check_output(("sudo -i -u hadoop /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.0-SNAPSHOT.jar wordcount {} input output").format(" ".join(["-D {}={}".format(property, value) for property, value in MAPREDUCE_PROPERTIES.items()])), stderr=subprocess.STDOUT, shell=True)
@@ -235,7 +243,7 @@ if __name__=="__main__":
             break
 
     sorted_logs = sorted_logs[reduce_start_index:]
-    # ----------------------------------------------------------------------------------- 
+    # -----------------------------------------------------------------------------------
 
     # print logs pertaining to shuffle and merge
     for line in sorted_logs:
@@ -257,4 +265,3 @@ if __name__=="__main__":
             print(line)
         else:
             continue
-
