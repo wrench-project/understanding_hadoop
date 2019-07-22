285125a553f3c67b511c24cb770767a972101cc5
ryan <ryanyt@hawaii.edu>
2019-07-22 13:19:40 -1000

    more docs

--- hadoop_mr_tests/reduce_merge_parts/get_strace_output.sh
+++ hadoop_mr_tests/reduce_merge_parts/get_strace_output.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# container id of most recently run container (the container use din this test)
+# Immediately after running this test, run this script to obtain
+# strace output obtained from the call of shuffleConsumerPlugin.run()
+# by the reduce task. 
+
+# container id of most recently run container (the container used in this test)
 CONTAINER_ID=$(docker ps -a -l --no-trunc | sed -n '2p' | cut -d' ' -f1)
 HOST_DEST_DIR="strace_output"
 CONTAINER_STRACE_OUTPUT_DIR="/home/hadoop/strace_output"
@@ -8,3 +12,7 @@ CONTAINER_STRACE_OUTPUT_DIR="/home/hadoop/strace_output"
 echo "Copying $CONTAINER_STRACE_OUTPUT from $CONTAINER_ID to $HOST_DEST_DIR"
 
 docker cp $CONTAINER_ID:$CONTAINER_STRACE_OUTPUT_DIR $HOST_DEST_DIR
+
+echo "File containing trace from merge located at:"
+
+grep -rl "writing/merging records" strace_output
